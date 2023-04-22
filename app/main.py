9a184cd3c11214932aaac3ef8e800a9d07277662
Juan Montalvo Bressi <juan-m12i@users.noreply.github.com>
2023-04-22 09:31:56 -0300

    working in local, not in docker

--- app/main.py
+++ app/main.py
@@ -1,19 +1,31 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
-from transformers import AutoTokenizer
 from typing import List
+from app.tokenizer import count_tokens
 
 app = FastAPI()
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
-tokenizer.model_max_length = 2048
 
-\@app.get("/")
+@app.get("/")
 def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return HTMLResponse("""
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Text Analysis Web App</title>
+                <link rel="stylesheet" href="/static/styles.css">
+            </head>
+            <body>
+                <h1>Text Analysis Web App</h1>
+                <form method="post" action="/count-tokens">
+                    <textarea name="text" placeholder="Enter text"></textarea>
+                    <button type="submit">Count Tokens</button>
+                </form>
+            </body>
+        </html>
+    """)
 
-\@app.post("/count-tokens")
-def count_tokens(request: Request,
-                text: str = Form(...)) -> List[int]:
-    tokens = tokenizer.encode(text)
-    return [len(tokens)]
+
+@app.post("/count-tokens")
+def count_tokens_route(text: str = Form(...)) -> List[int]:
+    return count_tokens(text)
 
