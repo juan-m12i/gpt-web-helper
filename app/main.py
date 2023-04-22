ff23c87e14385560b8e0a8c9c0526d119fbc2f6f
Juan Montalvo Bressi <juan-m12i@users.noreply.github.com>
2023-04-22 10:26:37 -0300

    working everywhere looking better, going for more

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from app.tokenizer import count_tokens
@@ -12,13 +12,11 @@ templates = Jinja2Templates(directory="templates")
 async def read_root(request: Request):
     return templates.TemplateResponse("count_tokens.html", {"request": request})
 
-@app.post("/")
+
+@app.post("/count_tokens")
 async def count(request: Request):
     form_data = await request.form()
     text = form_data["text"]
     token_count = count_tokens(text)
-    return templates.TemplateResponse("count_tokens.html", {"request": request, "text": text, "token_count": token_count})
-
-@app.get("/clear")
-async def clear(request: Request):
-    return templates.TemplateResponse("count_tokens.html", {"request": request})
+    response = {"token_count": token_count}
+    return JSONResponse(content=response)
--- static/script.js
+++ static/script.js
@@ -1,23 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const countButton = document.querySelector("#count-button");
-  const clearButton = document.querySelector("#clear-button");
-  const textInput = document.querySelector("#text-input");
-  const tokenCount = document.querySelector("#token-count");
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("#token-count-form");
+  const textarea = document.querySelector("textarea[name='text']");
+  const countBtn = document.querySelector("#count-tokens-btn");
+  const clearBtn = document.querySelector("#clear-btn");
+  const tokenCountMsg = document.querySelector("#token-count-msg");
 
-  countButton.addEventListener("click", async () => {
+  countBtn.addEventListener("click", async () => {
+    countBtn.disabled = true;
+    tokenCountMsg.style.display = "none";
+    clearBtn.style.display = "none";
     const response = await fetch("/", {
       method: "POST",
-      body: new FormData(document.querySelector("form")),
+      body: new FormData(form),
     });
-    const data = await response.text();
-    tokenCount.textContent = `The text has ${data} tokens.`;
-    textInput.disabled = true;
-    countButton.disabled = true;
+    const data = await response.json();
+    const tokenCount = data.token_count;
+    textarea.disabled = true;
+    tokenCountMsg.textContent = `The text has ${tokenCount} tokens.`;
+    tokenCountMsg.style.display = "block";
+    clearBtn.style.display = "block";
   });
 
-  clearButton.addEventListener("click", () => {
-    tokenCount.textContent = "";
-    textInput.disabled = false;
-    countButton.disabled = false;
+  clearBtn.addEventListener("click", () => {
+    textarea.disabled = false;
+    textarea.value = "";
+    countBtn.disabled = false;
+    tokenCountMsg.style.display = "none";
+    clearBtn.style.display = "none";
   });
 });
