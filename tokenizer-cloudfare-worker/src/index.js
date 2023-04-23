4ee45b2d97a63489b14343eb6fb5a6ff7886113a
Juan Montalvo Bressi <juan-m12i@users.noreply.github.com>
2023-04-23 15:03:49 -0300

    working static and cloudfare

--- tokenizer-cloudfare-worker/src/index.js
+++ tokenizer-cloudfare-worker/src/index.js
@@ -3,16 +3,35 @@ import wasm from "../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm";
 import model from "../node_modules/@dqbd/tiktoken/encoders/cl100k_base.json";
 
 export default {
-  async fetch() {
+  async fetch(request) {
     await init((imports) => WebAssembly.instantiate(wasm, imports));
     const encoder = new Tiktoken(
       model.bpe_ranks,
       model.special_tokens,
       model.pat_str
     );
-    const tokens = encoder.encode("hello world this is a test");
-    const numberOfTokens = tokens.length; // Get the length of the tokens array
+
+    let inputString = "";
+
+    const headers = new Headers({
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'text/plain'
+    });
+
+    if (request.method === "GET") {
+      // Extract the input string from the query parameter
+      const url = new URL(request.url);
+      inputString = url.searchParams.get("input") || "";
+    } else if (request.method === "POST") {
+      // Extract the input string from the request body
+      inputString = await request.text();
+    } else {
+      return new Response("Invalid request method", { status: 400 });
+    }
+
+    const tokens = encoder.encode(inputString);
+    const numberOfTokens = tokens.length;
     encoder.free();
-    return new Response(`${numberOfTokens}`); // Return the number of tokens
+    return new Response(`${numberOfTokens}`, { headers });
   },
 };
