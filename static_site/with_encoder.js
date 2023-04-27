7cf8b5684d9a46610775176a24d3ce9e88a00097
Juan <juan@tymit.com>
2023-04-27 14:37:12 -0300

    amended clear button logic

--- static_site/with_encoder.js
+++ static_site/with_encoder.js
@@ -3,16 +3,21 @@ const textInput = document.getElementById("text-input");
 const tokenCount = document.getElementById("token-count");
 const clearButton = document.getElementById("clear-btn");
 
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const text = textInput.value;
     const encoded = encode(text);
     tokenCount.textContent = `Token Count: ${encoded.length}`;
     textInput.disabled = true;
-});
+    clearBtn.disabled = false;
+  });
 
 clearButton.addEventListener("click", () => {
     textInput.disabled = false;
     textInput.value = "";
+    countTokensBtn.disabled = false;
+    clearBtn.disabled = true;
     tokenCount.textContent = "";
+    textInput.classList.remove("bg-gray-200");
 });
